6327e7ba44b5a692906f17c7ef6bb7e39556664f
matheushenriquereiter <mathenriquereiter@gmail.com>
2026-04-23 21:26:37 -0300

    Did two OOP exercises

---
+++ src/POO/Basico/exercicios/exercicio01/Pessoa.java
@@ -0,0 +1,15 @@
+package POO.Basico.exercicios.exercicio01;
+
+public class Pessoa {
+    String nome;
+    int idade;
+
+    public Pessoa(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public boolean ehMaiorDeIdade() {
+        return this.idade >= 18;
+    }
+}
---
+++ src/POO/Basico/exercicios/exercicio02/Main.java
@@ -0,0 +1,9 @@
+package POO.Basico.exercicios.exercicio02;
+
+public class Main {
+    public static void main(String[] args) {
+        Livro livro = new Livro("Livro do Jorge", "Jorge", 328, 21);
+
+        System.out.println(livro.ehLivroAntigo());
+    }
+}
